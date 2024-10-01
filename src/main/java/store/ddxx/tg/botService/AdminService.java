6f8fadd0336b95410637c4a2226c7efc38fc989e
Jahongir Akramov <jahongirakramov0000@gmail.com>
2024-10-01 14:42:10 +0500

    connect

--- src/main/java/store/ddxx/tg/botService/AdminService.java
+++ src/main/java/store/ddxx/tg/botService/AdminService.java
@@ -2,7 +2,12 @@ package store.ddxx.tg.botService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import store.ddxx.tg.model.ActiveChatUsers;
+import store.ddxx.tg.model.User;
+import store.ddxx.tg.service.ActiveChatUsersService;
 import store.ddxx.tg.service.UserService;
+
+import static store.ddxx.tg.model.UserState.*;
 
 @Component
 @RequiredArgsConstructor
@@ -10,6 +15,7 @@ public class AdminService {
 
     private final SendService send;
     private final UserService userService;
+    private final ActiveChatUsersService activeChatUsersService;
 
     public void sendText(String text){
 
@@ -18,6 +24,25 @@ public class AdminService {
                     Long.parseLong(text.substring(6, text.indexOf(" "))),
                     text.substring(text.indexOf(" ") + 1)
             );
+        } else if (text.startsWith("connect")) {
+            Long chatId1 = Long.parseLong(text.substring(7, text.indexOf("|")));
+            Long chatId2 = Long.parseLong(text.substring(text.indexOf("|") + 1));
+            User user1 = userService.findById(chatId1);
+            User user2 = userService.findById(chatId2);
+            if (user1.getUserState().equals(ACTIVATE)
+                && user2.getUserState().equals(ACTIVATE)) {
+                user1.setUserState(CHAT);
+                user2.setUserState(CHAT);
+                userService.save(user1);
+                userService.save(user2);
+                ActiveChatUsers  activeChatUsers = ActiveChatUsers.builder()
+                        .userId1(chatId1)
+                        .userId2(chatId2)
+                        .build();
+                activeChatUsersService.save(activeChatUsers);
+                send.controlReplyKeyboardMarkup(chatId1, "Suhbatni boshlashingiz mumkin!");
+                send.controlReplyKeyboardMarkup(chatId2, "Suhbatni boshlashingiz mumkin!");
+            }
         }
         else userService.findAllChatId()
                 .forEach(chatId -> send.sendTextMessage(chatId, text));
